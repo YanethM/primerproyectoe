3ea1eb4d1e4fc9611a50f6dd9e09b61342f3ea94
unknown <estuam@SALAF401-06.UAM.LOCAL>
2017-02-25 08:57:08 -0500

    Primer commit

---
+++ app/Http/Controllers/sitioController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class sitioController extends Controller
+{
+    //
+    public function index()
+    {
+     echo "Hola Mundo";
+    }
+
+    public function saludar(Request $request, $nombre)
+    {
+     echo "Hello " . $nombre;
+    }
+
+}
